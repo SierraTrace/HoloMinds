1ca7f1fb95a5ffd501abca7ed3e28c29e966be4b
CarrasDev <carrascoluque@gmail.com>
2026-04-08 20:10:25 +0200

    Crear Scene List

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,7 +4,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public int totalScore = 0;                  // Puntuación total acumulada por el jugador
+    public int totalScore;                  // Puntuación total acumulada por el jugador
     public int[] levelScores = new int[5];      // Guardaremos las puntuaciones de cada nivel
 
     void Awake()
