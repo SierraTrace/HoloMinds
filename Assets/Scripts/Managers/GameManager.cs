86befe8a69693323c7bbefa429b08f4e40488239
CarrasDev <carrascoluque@gmail.com>
2026-04-09 18:38:19 +0200

    Añadir funciones a GameManager para calcular los totales

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,7 +4,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    public int totalScore;                  // Puntuación total acumulada por el jugador
+    public int totalScore;                      // Puntuación total acumulada por el jugador
     public int[] levelScores = new int[5];      // Guardaremos las puntuaciones de cada nivel
 
     void Awake()
@@ -17,6 +17,24 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    public void AddScore(int levelIndex, int score)
+    {
+        if (levelIndex >= 0 && levelIndex < levelScores.Length)
+        {
+            levelScores[levelIndex] = score;
+            RecalculateTotal();
+        }
+    }
+
+    void RecalculateTotal()
+    {
+        totalScore = 0;
+        foreach (int score in levelScores)
+        {
+            totalScore += score;
         }
     }
 }
