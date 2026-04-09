4bebb5560375e1c52f8c098f9e4344324f0eee62
CarrasDev <carrascoluque@gmail.com>
2026-04-09 22:06:35 +0200

    Crear script FinalScoreUI

---
+++ Assets/Scripts/Core/FinalScoreUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class FinalScoreUI : MonoBehaviour
+{
+    public TextMeshProUGUI finalScoreText;
+
+    void Start()
+    {
+        finalScoreText.text = "Puntuación total: " + GameManager.Instance.totalScore;
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneLoader.LoadMainMenu();
+    }
+}
