1605f0233e514542b82a73e54ca2c847f8a7dfe9
CarrasDev <carrascoluque@gmail.com>
2026-04-17 19:16:46 +0200

    Aplicar anima cion correr por defecto y saltar

--- Assets/Scripts/Minigames/Game2/PlayerJump.cs
+++ Assets/Scripts/Minigames/Game2/PlayerJump.cs
@@ -3,39 +3,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerJump : MonoBehaviour
 {
-    public float jumpForce = 12f;
+    public float jumpForce = 20f;
     private Rigidbody2D rb;
     private bool isGrounded = false;
     private InputAction jumpAction;
 
+    private Animator animator;             // Referencia al componente Animator para controlar las animaciones  
+
     public MinigameEnd endController;      // Referencia al script que maneja el fin del minijuego
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        jumpAction = new InputAction(type: InputActionType.Button);
+        animator = GetComponent<Animator>();
 
+        jumpAction = new InputAction(type: InputActionType.Button);
         jumpAction.AddBinding("<Keyboard>/space");                  // Salto en PC o Mac
         jumpAction.AddBinding("<Touchscreen>/primaryTouch/press");  // Salto en movil
 
         jumpAction.performed += ctx => TryJump();        
     }
 
-    private void OnEnable()
+    private void Start()
     {
-        jumpAction.Enable();
+        animator.SetBool("isRun", true);    // Iniciamos corriendo
+        animator.SetBool("isJump", false);
     }
 
-    private void OnDisable()
-    {
-        jumpAction.Disable();
-    }
+    private void OnEnable() => jumpAction.Enable();
+    private void OnDisable() => jumpAction.Disable();
+
 
     private void TryJump()
     {
         if (isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+            animator.SetBool("isRun", false);
+            animator.SetBool("isJump", true);
         }
     }
 
@@ -44,6 +51,9 @@ public class PlayerJump : MonoBehaviour
         if (collision.collider.CompareTag("Ground"))
         {
             isGrounded = true;
+
+            animator.SetBool("isRun", true);
+            animator.SetBool("isJump", false);
         }
 
         if (collision.collider.CompareTag("Obstacle"))
@@ -54,6 +64,12 @@ public class PlayerJump : MonoBehaviour
 
     private void HandleFinJuego()
     {
+        //Animación de muerte
+        animator.SetTrigger("Die");
+        animator.SetBool("isRun", false);
+        animator.SetBool("isJump", false);
+        
+
         if (ScoreManager.Instance != null)
         {
             ScoreManager.Instance.StopScore();
@@ -77,6 +93,11 @@ public class PlayerJump : MonoBehaviour
         {
             Debug.LogError("No se ha asignado el EndController al script PlayerJump.");
         }
+
+        this.enabled = false;                   // Desactivar este script para evitar más interacciones
+        rb.simulated = false;                    // Detener la física del jugador
+
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
