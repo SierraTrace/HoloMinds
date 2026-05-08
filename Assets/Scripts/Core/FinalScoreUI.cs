546f49876d352e162ab35deddd7507e3e1474d88
CarrasDev <carrascoluque@gmail.com>
2026-05-08 18:28:48 +0200

    Crear corrutina para mostrar las puntuaciones finales

--- Assets/Scripts/Core/FinalScoreUI.cs
+++ Assets/Scripts/Core/FinalScoreUI.cs
@@ -1,14 +1,50 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class FinalScoreUI : MonoBehaviour
 {
-    public TextMeshProUGUI finalScoreText;
+    [Header("Referencias de UI")]
+    public TextMeshProUGUI scoresListText;
+    public TextMeshProUGUI totalScoreText;
+    public GameObject mainMenuButton;
+
+    [Header("Configuración")]
+    public float delayBetweenScores = 0.8f;
 
     void Start()
     {
-        finalScoreText.text = "Puntuación total: " + GameManager.Instance.totalScore;
+        scoresListText.text = "";
+        totalScoreText.text = "";
+        mainMenuButton.SetActive(false);
+
+        StartCoroutine(ShowScoresSequence());
+
     }
+
+
+    private IEnumerator ShowScoreSequence()
+    {
+        int[] scores = GameManager.Instance.gameScores;
+
+        for(int i = 0; i < scores.Length; i++)
+        {
+            yield return new WaitForSeconds(delayBetweenScores);
+
+            scoresListText.text += $"Juego {i + 1}: {scores[i]} pts\n";
+
+            // TODO: Llamada a sonido de puntuación aquí
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        totalScoreText.text = "Puntuación Total: " + {GameManager.Instance.totalScore};
+
+        yield return new WaitForSeconds(0.5f);
+
+        mainMenuButton.SetActive(true);
+    }
+
 
     public void BackToMainMenu()
     {
